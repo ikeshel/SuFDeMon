c64c54dad8d23aafa037e63cfe610037fb417643
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-19 21:08:01 +0200

    Add common project foundation

---
+++ common/inc/SupFDetMonProtocol.h
@@ -0,0 +1,19 @@
+#ifndef SUPFDETMON_PROTOCOL_H
+#define SUPFDETMON_PROTOCOL_H
+
+#include <string_view>
+
+namespace SupFDetMon::Protocol {
+
+inline constexpr int kDefaultPort = 9090;
+
+inline constexpr std::string_view kPing = "PING";
+inline constexpr std::string_view kList = "LIST";
+inline constexpr std::string_view kGet = "GET";
+inline constexpr std::string_view kClear = "CLEAR";
+inline constexpr std::string_view kClearAll = "CLEAR ALL";
+inline constexpr std::string_view kQuit = "QUIT";
+
+} // namespace SupFDetMon::Protocol
+
+#endif
