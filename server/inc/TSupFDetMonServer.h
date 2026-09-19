20e8ac24bd42d4b5ae5058df1ea55c06f18fc424
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-19 21:10:30 +0200

    Implement initial SupFDetMon server

---
+++ server/inc/TSupFDetMonServer.h
@@ -0,0 +1,45 @@
+#ifndef TSUPFDETMONSERVER_H
+#define TSUPFDETMONSERVER_H
+
+#include <array>
+#include <memory>
+#include <string>
+
+class TH1D;
+class TRandom3;
+class TServerSocket;
+class TSocket;
+
+class TSupFDetMonServer
+{
+public:
+    explicit TSupFDetMonServer(int port);
+    ~TSupFDetMonServer();
+
+    TSupFDetMonServer(const TSupFDetMonServer&) = delete;
+    TSupFDetMonServer& operator=(const TSupFDetMonServer&) = delete;
+
+    int Run();
+
+private:
+    static constexpr int kNFieldCages = 3;
+    static constexpr int kNAdcChannels = 32;
+
+    using HistogramRow = std::array<std::unique_ptr<TH1D>, kNAdcChannels>;
+    using HistogramArray = std::array<HistogramRow, kNFieldCages>;
+
+    void CreateHistograms();
+    void FillHistograms();
+    bool HandleClient(TSocket& socket);
+    bool HandleCommand(TSocket& socket, const std::string& command);
+
+    TH1D* FindHistogram(const std::string& name);
+    std::string HistogramList() const;
+
+    int fPort;
+    HistogramArray fMusicAdc;
+    std::unique_ptr<TRandom3> fRandom;
+    std::unique_ptr<TServerSocket> fServerSocket;
+};
+
+#endif
