57be7819de98623821fd32bec373d96bd7bc1647
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-19 21:14:26 +0200

    Implement initial SupFDetMon client

---
+++ client/inc/TSupFDetMonClient.h
@@ -0,0 +1,39 @@
+#ifndef TSUPFDETMONCLIENT_H
+#define TSUPFDETMONCLIENT_H
+
+#include <memory>
+#include <string>
+
+class TH1D;
+class TSocket;
+
+class TSupFDetMonClient
+{
+public:
+    TSupFDetMonClient(std::string host, int port);
+    ~TSupFDetMonClient();
+
+    TSupFDetMonClient(const TSupFDetMonClient&) = delete;
+    TSupFDetMonClient& operator=(const TSupFDetMonClient&) = delete;
+
+    bool Connect();
+    void Disconnect();
+    bool IsConnected() const;
+
+    bool Ping();
+    std::string ListHistograms();
+    std::unique_ptr<TH1D> GetHistogram(const std::string& name);
+    bool ClearHistogram(const std::string& name);
+    bool ClearAll();
+    bool DrawHistogram(const std::string& name);
+
+private:
+    bool SendCommand(const std::string& command);
+    std::string ReceiveText();
+
+    std::string fHost;
+    int fPort;
+    std::unique_ptr<TSocket> fSocket;
+};
+
+#endif
