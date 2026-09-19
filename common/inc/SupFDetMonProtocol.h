8dcbbe17eeb0400c678103c03163eb4ae6c05412
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-19 23:20:10 +0200

    Fix accidental literal newline escapes

--- common/inc/SupFDetMonProtocol.h
+++ common/inc/SupFDetMonProtocol.h
@@ -12,7 +12,8 @@ inline constexpr std::string_view kList = "LIST";
 inline constexpr std::string_view kGet = "GET";
 inline constexpr std::string_view kClear = "CLEAR";
 inline constexpr std::string_view kClearAll = "CLEAR ALL";
-inline constexpr std::string_view kQuit = "QUIT";\ninline constexpr std::string_view kShutdown = "SHUTDOWN";
+inline constexpr std::string_view kQuit = "QUIT";
+inline constexpr std::string_view kShutdown = "SHUTDOWN";
 
 } // namespace SupFDetMon::Protocol
 
