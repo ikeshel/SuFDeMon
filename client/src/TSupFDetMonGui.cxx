e937382603e546742192415da26b3be4327b2646
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-19 22:07:17 +0200

    Separate GUI controls canvas and add ROOT Rint

--- client/src/TSupFDetMonGui.cxx
+++ client/src/TSupFDetMonGui.cxx
@@ -5,26 +5,23 @@
 
 #include <TCanvas.h>
 #include <TGButton.h>
-#include <TGClient.h>
 #include <TGComboBox.h>
 #include <TGFrame.h>
 #include <TGLabel.h>
 #include <TGNumberEntry.h>
 #include <TGTextEntry.h>
-#include <TRootEmbeddedCanvas.h>
 #include <TH1D.h>
 
-#include <iostream>
 #include <string>
 
 ClassImp(TSupFDetMonGui)
 
 TSupFDetMonGui::TSupFDetMonGui(const TGWindow* parent, UInt_t width, UInt_t height,
-                                   std::string host, int port)
+                               std::string host, int port)
     : TGMainFrame(parent, width, height)
 {
     SetCleanup(kDeepCleanup);
-    SetWindowName("SupFDetMon Client");
+    SetWindowName("SupFDetMon Controls");
 
     auto* connection = new TGGroupFrame(this, "Connection", kHorizontalFrame);
     fHostEntry = new TGTextEntry(connection, host.c_str());
@@ -36,21 +33,16 @@ TSupFDetMonGui::TSupFDetMonGui(const TGWindow* parent, UInt_t width, UInt_t heig
     fDisconnectButton = new TGTextButton(connection, "&Disconnect");
     fStatusLabel = new TGLabel(connection, "Disconnected");
 
-    connection->AddFrame(new TGLabel(connection, "Host:"),
-                         new TGLayoutHints(kLHintsCenterY | kLHintsLeft, 5, 4, 5, 5));
+    connection->AddFrame(new TGLabel(connection, "Host:"), new TGLayoutHints(kLHintsCenterY, 5, 4, 5, 5));
     connection->AddFrame(fHostEntry, new TGLayoutHints(kLHintsCenterY, 0, 15, 5, 5));
-    connection->AddFrame(new TGLabel(connection, "Port:"),
-                         new TGLayoutHints(kLHintsCenterY, 0, 4, 5, 5));
+    connection->AddFrame(new TGLabel(connection, "Port:"), new TGLayoutHints(kLHintsCenterY, 0, 4, 5, 5));
     connection->AddFrame(fPortEntry, new TGLayoutHints(kLHintsCenterY, 0, 15, 5, 5));
     connection->AddFrame(fConnectButton, new TGLayoutHints(kLHintsCenterY, 0, 8, 5, 5));
     connection->AddFrame(fDisconnectButton, new TGLayoutHints(kLHintsCenterY, 0, 20, 5, 5));
     connection->AddFrame(fStatusLabel, new TGLayoutHints(kLHintsCenterY, 0, 5, 5, 5));
     AddFrame(connection, new TGLayoutHints(kLHintsExpandX, 8, 8, 8, 4));
 
-    auto* body = new TGHorizontalFrame(this);
-
-    auto* controls = new TGGroupFrame(body, "Select Histogram", kVerticalFrame);
-    controls->Resize(300, 500);
+    auto* controls = new TGGroupFrame(this, "Select Histogram", kVerticalFrame);
 
     auto* fcRow = new TGHorizontalFrame(controls);
     fcRow->AddFrame(new TGLabel(fcRow, "Field Cage:"), new TGLayoutHints(kLHintsCenterY, 2, 8, 4, 4));
@@ -58,7 +50,7 @@ TSupFDetMonGui::TSupFDetMonGui(const TGWindow* parent, UInt_t width, UInt_t heig
     for (int fc = 1; fc <= SupFDetMon::kNFieldCages; ++fc)
         fFieldCageCombo->AddEntry(("FC" + std::to_string(fc)).c_str(), fc);
     fFieldCageCombo->Select(1);
-    fFieldCageCombo->Resize(150, 24);
+    fFieldCageCombo->Resize(160, 24);
     fcRow->AddFrame(fFieldCageCombo, new TGLayoutHints(kLHintsExpandX, 2, 2, 4, 4));
     controls->AddFrame(fcRow, new TGLayoutHints(kLHintsExpandX));
 
@@ -68,12 +60,11 @@ TSupFDetMonGui::TSupFDetMonGui(const TGWindow* parent, UInt_t width, UInt_t heig
     for (int adc = 0; adc < SupFDetMon::kNAdcChannels; ++adc)
         fAdcCombo->AddEntry(("ADC" + std::to_string(adc)).c_str(), adc);
     fAdcCombo->Select(0);
-    fAdcCombo->Resize(150, 24);
+    fAdcCombo->Resize(160, 24);
     adcRow->AddFrame(fAdcCombo, new TGLayoutHints(kLHintsExpandX, 2, 2, 4, 4));
     controls->AddFrame(adcRow, new TGLayoutHints(kLHintsExpandX));
 
-    controls->AddFrame(new TGLabel(controls, "Histogram name:"),
-                       new TGLayoutHints(kLHintsLeft, 2, 2, 12, 2));
+    controls->AddFrame(new TGLabel(controls, "Histogram name:"), new TGLayoutHints(kLHintsLeft, 2, 2, 12, 2));
     fHistogramEntry = new TGTextEntry(controls);
     fHistogramEntry->SetEnabled(kFALSE);
     controls->AddFrame(fHistogramEntry, new TGLayoutHints(kLHintsExpandX, 2, 2, 2, 10));
@@ -83,16 +74,8 @@ TSupFDetMonGui::TSupFDetMonGui(const TGWindow* parent, UInt_t width, UInt_t heig
     fClearAllButton = new TGTextButton(controls, "Clear &All");
     controls->AddFrame(fDrawButton, new TGLayoutHints(kLHintsExpandX, 2, 2, 8, 4));
     controls->AddFrame(fClearButton, new TGLayoutHints(kLHintsExpandX, 2, 2, 4, 4));
-    controls->AddFrame(fClearAllButton, new TGLayoutHints(kLHintsExpandX, 2, 2, 4, 4));
-
-    body->AddFrame(controls, new TGLayoutHints(kLHintsLeft | kLHintsExpandY, 8, 4, 4, 8));
-
-    auto* display = new TGGroupFrame(body, "Histogram", kVerticalFrame);
-    fCanvas = new TRootEmbeddedCanvas("SupFDetMonCanvas", display, 850, 560);
-    display->AddFrame(fCanvas, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 4, 4, 4, 4));
-    body->AddFrame(display, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 4, 8, 4, 8));
-
-    AddFrame(body, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY));
+    controls->AddFrame(fClearAllButton, new TGLayoutHints(kLHintsExpandX, 2, 2, 4, 8));
+    AddFrame(controls, new TGLayoutHints(kLHintsExpandX | kLHintsExpandY, 8, 8, 4, 8));
 
     fConnectButton->Connect("Clicked()", "TSupFDetMonGui", this, "ConnectServer()");
     fDisconnectButton->Connect("Clicked()", "TSupFDetMonGui", this, "DisconnectServer()");
@@ -117,9 +100,7 @@ TSupFDetMonGui::~TSupFDetMonGui()
 
 std::string TSupFDetMonGui::SelectedHistogramName() const
 {
-    const int fc = fFieldCageCombo->GetSelected();
-    const int adc = fAdcCombo->GetSelected();
-    return SupFDetMon::MusicAdcHistogramName(fc, adc);
+    return SupFDetMon::MusicAdcHistogramName(fFieldCageCombo->GetSelected(), fAdcCombo->GetSelected());
 }
 
 void TSupFDetMonGui::UpdateHistogramName()
@@ -141,11 +122,8 @@ void TSupFDetMonGui::SetConnectedUi(bool connected)
 void TSupFDetMonGui::ConnectServer()
 {
     if (fClient) fClient->Disconnect();
-
-    const std::string host = fHostEntry->GetText();
-    const int port = static_cast<int>(fPortEntry->GetNumber());
-
-    fClient = std::make_unique<TSupFDetMonClient>(host, port);
+    fClient = std::make_unique<TSupFDetMonClient>(fHostEntry->GetText(),
+                                                  static_cast<int>(fPortEntry->GetNumber()));
     const bool connected = fClient->Connect();
     if (!connected) fClient.reset();
     SetConnectedUi(connected);
@@ -172,12 +150,14 @@ void TSupFDetMonGui::DrawSelected()
     fHistogram = fClient->GetHistogram(SelectedHistogramName());
     if (!fHistogram) return;
 
-    TCanvas* canvas = fCanvas->GetCanvas();
-    canvas->cd();
-    canvas->Clear();
+    if (!fCanvas)
+        fCanvas = new TCanvas("SupFDetMonCanvas", "SupFDetMon Histogram", 1000, 700);
+
+    fCanvas->cd();
+    fCanvas->Clear();
     fHistogram->Draw();
-    canvas->Modified();
-    canvas->Update();
+    fCanvas->Modified();
+    fCanvas->Update();
 }
 
 void TSupFDetMonGui::ClearSelected()
