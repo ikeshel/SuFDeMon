24a19d6d2ea2f28a21cff38abc02bcb5ba0c521a
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-20 12:04:43 +0200

    Add standardized project and license headers

--- common/inc/SupFDetMonNames.h
+++ common/inc/SupFDetMonNames.h
@@ -1,3 +1,15 @@
+// Author: Irakli Keshelashvili, 2026
+//
+// SupFDetMon - Super-FRS Detector Monitoring Software
+//
+// Copyright (C) 2026 Irakli Keshelashvili
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, version 3.
+//
+// See the LICENSE file in the project root for the full license text.
+
 #ifndef SUPFDETMON_NAMES_H
 #define SUPFDETMON_NAMES_H
 
