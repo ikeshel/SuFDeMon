4da3d974fd1cbbdd3863a56dae28892964179ea7
Irakli Keshelashvili <i.keshelashvili@icloud.com>
2026-09-24 21:17:41 +0200

    Fix ROOT GUI dictionary generation with explicit LinkDef

---
+++ client/inc/SuFDeMonGuiLinkDef.h
@@ -0,0 +1,10 @@
+#ifdef __CLING__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+// Enable ROOT reflection for the GUI's signal/slot callbacks.
+#pragma link C++ class TSuFDeMonGui+;
+
+#endif
